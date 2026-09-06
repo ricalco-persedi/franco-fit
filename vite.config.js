fdd5dedf48dd19ccc3050e177f392046a2145253
IA Chef <ricardoalbertocomeglio@gmail.com>
2026-09-06 18:01:21 -0300

    feat: Adecuacion de arquitectura a tailwind v.4

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   root: './',
   base: './',
+  plugins: [
+    tailwindcss(),
+  ],
   build: {
     outDir: 'dist',
     sourcemap: false,
